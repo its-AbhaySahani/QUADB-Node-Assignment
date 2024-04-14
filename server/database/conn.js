989d5600decb781f36868c95f1a7574f2c1dc5e2
Abhay Sahani <abhay.cs.777@gmail.com>
2024-04-14 20:23:41 +0530

    updated conn.js

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,12 +1,19 @@
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sajc8ea.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://abhay12115826:gzLvZLHvPAq52SEo@quadb.v3omskc.mongodb.net/?retryWrites=true&w=majority&appName=QUADB`;
 
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverApiVersion: ServerApiVersion.v1,
+});
+
+client.connect(err => {
+    if (err) {
+        console.error('Failed to connect to MongoDB', err);
+    } else {
+        console.log('Successfully connected to MongoDB');
+    }
 });
 
 module.exports = client;
